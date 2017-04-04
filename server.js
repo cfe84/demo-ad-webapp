51233f3f49587f3197f516edfdc03ae6edb69a55
Charles Feval <github@crap.feval.fr>
2017-04-03 17:04:09 -0700

    Adding parsing of jwt for roles and groups

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwtDecode = require("jwt-decode");
 
 const PORT = process.env.PORT || 8080;
 
@@ -7,6 +8,15 @@ var app = new express();
 app.get("/", (req, res) => {
   for (var name in req.headers)
     res.write(`${name}: ${req.headers[name]}\n`);
+  var token;
+  if (token = req.headers["x-ms-token-aad-id-token"])
+  {
+    var token = jwtDecode(token);
+    if (token.groups)
+      token.groups.forEach(group => console.log(`You are part of group ${group}`));
+    if (token.roles)
+      token.roles.forEach(role => console.log(`You have role ${role}`));
+  }
   res.end();
 });
 
